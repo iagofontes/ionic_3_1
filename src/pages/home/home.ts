af16d48a3e1d5df9e4efc81844b44c5ebf8b4925
iagofontes <iagofontes@hotmail.com>
2018-04-29 23:54:35 -0300

    commit inicial, listagem ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros;
 
+  constructor(public navCtrl: NavController) {
+    this.carros = [
+      {nome:'Fusca', preco:2000},
+      {nome:'Fox', preco:15000},
+      {nome:'Gol G6', preco:20000},
+      {nome:'Onix', preco:35000},
+      {nome:'Pajeto TR4', preco:46250},
+      {nome:'Doblo', preco:21850},
+      {nome:'HB20', preco:39000},
+      {nome:'Polo Sedan', preco:24500},
+      {nome:'Voyage', preco:14200},
+      {nome:'Celta', preco:15632},
+      {nome:'Maseratti', preco:1250000},
+    ];
   }
 
 }
